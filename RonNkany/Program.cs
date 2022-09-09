249f3454f460d9f19d18b943a3f7cb5bc424c9b3
ihababoudiab@gmail.com <ihababoudiab@gmail.com>
2022-09-09 13:09:45 -0700

    first

---
+++ RonNkany/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace RonNkanye
+{
+     class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+            for (int i = 0;  i< 5; i++)
+            {
+
+             var Kanye = new HttpClient();
+             var kanyeURL = "https://api.kanye.rest/";
+             var kanyeResponse = Kanye.GetStringAsync(kanyeURL).Result;
+             var KanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+             Console.WriteLine(KanyeQuote);
+             Console.WriteLine();
+
+             string RonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+             string RonResponse = Kanye.GetStringAsync(RonURL).Result;
+             var RonObject = JArray.Parse(RonResponse);
+             Console.WriteLine(RonObject[0]);
+
+
+
+            };
+
+
+
+        }
+    }
+}
