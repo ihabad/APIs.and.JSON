e26c16fe03a587685156c62f1b1b97068813d6c7
ihababoudiab@gmail.com <ihababoudiab@gmail.com>
2022-09-09 13:26:10 -0700

    first commit

--- RonNkany/Program.cs
+++ RonNkany/Program.cs
@@ -11,26 +11,23 @@ namespace RonNkanye
 
         static void Main(string[] args)
         {
+             var client = new HttpClient();
+
             for (int i = 0;  i< 5; i++)
             {
 
-             var Kanye = new HttpClient();
-             var kanyeURL = "https://api.kanye.rest/";
-             var kanyeResponse = Kanye.GetStringAsync(kanyeURL).Result;
+             var KanyeURL = "https://api.kanye.rest/";
+             var kanyeResponse = client.GetStringAsync(KanyeURL).Result;
              var KanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
              Console.WriteLine(KanyeQuote);
              Console.WriteLine();
 
              string RonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
-             string RonResponse = Kanye.GetStringAsync(RonURL).Result;
+             string RonResponse = client.GetStringAsync(RonURL).Result;
              var RonObject = JArray.Parse(RonResponse);
              Console.WriteLine(RonObject[0]);
 
-
-
             };
-
-
 
         }
     }
